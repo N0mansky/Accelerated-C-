d15f32d27f87e6eca8911d73b3c4febba00b2a30
n0mansky <fueuru@163.com>
2020-08-26 10:19:43 +0800

    test

---
+++ chapter0/0_4.cpp
@@ -0,0 +1,11 @@
+// Write a program that, when run, writes the hello world program as its output.
+#include <iostream>
+
+int main(){
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << "int main(){" << std::endl;
+    std::cout << "std::count << \"hello,world\" << std::endl;" << std::endl;
+    std::cout << "return 0;" << std::endl;
+    std::cout << "}" << std::endl;
+    return 0;
+}
